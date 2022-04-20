a50402da0bfbfb21a21d565a5af1aa611791a529
Jahid Hasan <vdjsovaj@gmail.com>
2022-04-20 20:03:44 +0600

    subs page design

---
+++ app/src/main/java/dev/JahidHasanCo/DevMuscles/presentation/activity/SubscriptionActivity.kt
@@ -0,0 +1,12 @@
+package dev.JahidHasanCo.DevMuscles.presentation.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.JahidHasanCo.DevMuscles.R
+
+class SubscriptionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_subscription)
+    }
+}
